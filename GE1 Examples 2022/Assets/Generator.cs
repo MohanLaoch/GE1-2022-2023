8b0256c3c4a97afb83f64a8eae3cdf96d6d2f2cf
James Mohan <c20305011@mytudublin.ie>
2022-09-30 17:11:40 +0100

    test

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -9,11 +9,15 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for (var i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
