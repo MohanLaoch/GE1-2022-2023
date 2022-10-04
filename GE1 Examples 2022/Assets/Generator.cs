c13c5db0dd9aee1b6f0849b7540c3158cba95e8e
James Mohan <c20305011@mytudublin.ie>
2022-10-04 19:21:20 +0100

    First circle added

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    public int loops = 10;
+    //public int loops = 10;
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
-        for (var i = 0; i < 10; i++)
+        /*for (var i = 0; i < 10; i++)
         {
             Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+        }*/
+
+        Vector3 targetPosition = Vector3.zero;
+        
+        for (var i = 0; i < 10; i++)
+        {
+            GameObject instance = Instantiate(prefab);
+
+            float angle = i * (2 * 3.14f / 10);
+
+            float x = Mathf.Cos(angle) * 1.5f;
+            float y = Mathf.Sin(angle) * 1.5f;
+
+            targetPosition = new Vector3(targetPosition.x + x, targetPosition.y + y, 0);
+
+            instance.transform.position = targetPosition;
         }
     }
 
