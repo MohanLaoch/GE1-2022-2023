b3edd5254a8898d3b16ed25aa3181253562a97c6
James Mohan <c20305011@mytudublin.ie>
2022-10-07 16:58:26 +0100

    Variables

---
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Transform head;
+    public Transform tail;
+
+    [Range(0, 1f)]
+    public float frequencey;
+
+    public int headAmplitude;
+    public int tailAmplitude;
+
+    public float Theta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
