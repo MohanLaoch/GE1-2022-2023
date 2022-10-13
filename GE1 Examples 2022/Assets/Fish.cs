35f510bad66973928af114ce1891c511195b47e0
James Mohan <c20305011@mytudublin.ie>
2022-10-13 20:50:50 +0100

    Rotating Fish

--- GE1 Examples 2022/Assets/Fish.cs
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -7,23 +7,32 @@ public class Fish : MonoBehaviour
     public Transform head;
     public Transform tail;
 
-    [Range(0, 1f)]
-    public float frequencey;
+    [Range(0.0f, 5f)]
+    public float frequency = 0.5f;
 
-    public int headAmplitude;
-    public int tailAmplitude;
+    public float headAmplitude = 40;
+    public float tailAmplitude = 60;
 
-    public float Theta;
+    public float theta;
 
     // Start is called before the first frame update
     void Start()
     {
+        theta = 0;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        theta = theta + 0.1f;
+
+        head.transform.localRotation = Quaternion.AngleAxis(headAmplitude * Mathf.Sin(theta * frequency), Vector3.forward);
+        tail.transform.localRotation = Quaternion.AngleAxis(tailAmplitude * Mathf.Sin(theta * frequency), Vector3.forward);
     }
+    /*
+     * Mathf.Sin
+     * Quaternion.AngleAxis
+     * transform.localRotation
+     */
 }
